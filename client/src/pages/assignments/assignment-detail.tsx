66d41e8a2198531bc68a2edfae2b8fcbb56bac01
migkjy <migkjy@naver.com>
2025-03-02 04:12:25 +0000

    Restored to '025a30ed99d6c4f9f99eca6de32b7abcd28a1827'
    Replit-Restored-To: 025a30ed99d6c4f9f99eca6de32b7abcd28a1827

--- client/src/pages/assignments/assignment-detail.tsx
+++ client/src/pages/assignments/assignment-detail.tsx
@@ -318,7 +318,7 @@ export default function AssignmentDetail() {
                                 {
                                   students?.find(
                                     (s) => s.id === submission.studentId,
-                                  )?.name || "Unknown Student"
+                                  )?.name
                                 }
                               </TableCell>
                               <TableCell>
